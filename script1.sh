d105547bdea00e0e96dda1e09bf8efdfd5ebf279
CS <62691147+panrajucs@users.noreply.github.com>
2020-10-15 23:44:22 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 echo "Starting script"
-
+apt update
+curl https://get.docker.com/ | bash
 sed -i "/ExecStart=/c\ExecStart=/usr/bin/dockerd -H fd:// --containerd=/run/containerd/containerd.sock -H unix:// -H tcp://0.0.0.0:2375" /lib/systemd/system/docker.service
 
 systemctl daemon-reload
